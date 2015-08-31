665dff93a7af0834205ddffa0a2fac420ccd4b37
Matthieu FOURNET <fournet.matthieu@gmail.com>
2015-08-31 21:47:45 +0200

    Add the warm up countdown, the pace monitor, and clean up the code

--- SEUIL_3x2000m_r2min.js
+++ SEUIL_3x2000m_r2min.js
@@ -1,5 +1,7 @@
 /*
 ######################################### SEUIL 3x2000m r2min #######################################
+# version : 20150831
+#
 # DESCRIPTION :
 # 	long training (no duration/distance specified)
 # 	Includes 3 fast runs in which :
@@ -11,132 +13,163 @@
 #
 # 	WARM UP :
 # 		Upon starting exercice, the app is in "Warmup" mode, until the 'LAP' button is pressed for the 1st fast run.
+# 		After the 'warmUpMinimumDurationMinutes' is over, the watch displays 'H 0 T'
 #
 # 	RUNS :
 # 		The watch displays "1 RUN" during the 1st fast run, "2 RUN" during the 2nd fast run, and so on.
 #
 # 	PACE CONTROL :
-# 		If running too fast (target pace + margin%), the display will be " n Run-".
-# 		If running too slow, the display will be " n Run+".
+#		The built-in pace monitor will warn by displaying if the running pace (compared to target pace +/- margin%) is :
+#			- too fast		: "RUN n --"
+#			- within specs	: "RUN n"
+#			- too slow 		: "RUN n ++"
 #
 # 	RESTS :
-# 		During rests, the watch displays the number of remaining seconds : " R n s".
+# 		During rests, the watch displays the number of remaining seconds : " RST n s".
 #
 # 	CALM DOWN :
 # 		After the last run, the watch displays "CALM".
 #
 # VARIABLES :
-# 	fastRunLengthMeters = 2000		can be edited
-# 	restBetweenRunsSeconds = 120	can be edited
+# 	warmUpMinimumDurationMinutes = 15	can be edited
+# 	runLengthMeters = 2000				can be edited
+# 	restBetweenRunsSeconds = 120		can be edited
 #
-# 	==> this declares the target run pace as 5:15 min/km (=5.25min/km)
-# 	targetPacePerKmMinutes = 5		can be edited
-# 	targetPacePerKmSeconds = 15		can be edited
-# 	targetPace = 0					don't edit
-# 	paceAlertTooFast = 0			don't edit
-# 	paceAlertTooSlow = 0			don't edit
+# 	==> this declares the target run pace as 4min40s/km
+# 	targetPacePerKmMinutes = 4			can be edited
+# 	targetPacePerKmSeconds = 40			can be edited
+# 	targetPace = 0						don't edit
+# 	paceAlertTooFast = 0				don't edit
+# 	paceAlertTooSlow = 0				don't edit
 #
-# 	paceMarginPercent = 8			can be edited. Means "OK if running within +/-8% of target pace".
-# 									With margin = 8% and target pace = 5:15min/km, fastest = 4:49, slowest = 5:40
+# 	paceMarginPercent = 8				can be edited. Means "OK if running within +/-8% of target pace".
+# 										With margin = 8% and target pace = 5:15min/km, fastest = 4:49, slowest = 5:40
 #
-# 	step = 0						don't edit
-# 	myDurationSeconds = 0			don't edit
-# 	myDistanceKm = 0				don't edit
-# 	restTimeLeft = 0				don't edit
-# 	myResultVar = 0					don't edit
-# 	runId = 1						don't edit
+# 	step = 0							don't edit
+# 	myDurationSeconds = 0				don't edit
+# 	myDistanceKm = 0					don't edit
+# 	secondsLeft = 0						don't edit
+# 	myResultVar = 0						don't edit
+# 	runId = 1							don't edit
+# 	endOfStepSeconds = 0				don't edit
+# 	endOfStepKm = 0						don't edit
 #
 # 	==> Don't forget to set the result format to 0 decimal.
 #
 # SUGGESTED WATCH SCREEN CONFIGURATION :
-#	- TODO
+#	- pace
+#	- this app
+#	- distance
 ########################################## ##########################################################
 */
 
-
 /* While in sport mode do this once per second */
-prefix="";
-RESULT=myResultVar;
-postfix="";
+prefix  = "";
+RESULT  = myResultVar;
+postfix = "";
 
-if (step<1) {
-	/* WARM UP */
-	prefix="W";
-	myResultVar=0;
-	postfix="UP";
-	/* Simulate pressing the "LAP" watch button */
-	if (SUUNTO_LAP_NUMBER > 1) {
-		Suunto.alarmBeep();
-		step=1;
 
-		/* initialize values for pace monitoring */
-		targetPace=targetPacePerKmMinutes+(targetPacePerKmSeconds/60);
-		paceAlertTooFast=targetPace*(100-paceMarginPercent)/100;	/* these are minutes/km, so the lower the value, the faster you run */
-		paceAlertTooSlow=targetPace*(100+paceMarginPercent)/100;	/* ...and vice-versa ;-) */
+/***********
+ * WARM UP *
+ **********/
+if (step < 1) {
+	endOfStepSeconds = warmUpMinimumDurationMinutes * 60;
 
-		myDurationSeconds=SUUNTO_DURATION;
-		myDistanceKm=SUUNTO_DISTANCE;
+	/* IS THE WARM-UP OVER ? */
+	if (SUUNTO_DURATION > endOfStepSeconds) {
+		/* YES */
+		prefix = "H";
+		myResultVar = 0;
+		postfix = "T";	/* ==> 'HOT' ;-) */
+
+		/* Press the "LAP" watch button to go for the first run */
+		if (SUUNTO_LAP_NUMBER > 1) {
+			Suunto.alarmBeep();
+			step = 1;
+
+			/* initialize values for pace monitoring */
+			targetPace = targetPacePerKmMinutes + (targetPacePerKmSeconds / 60);
+			paceAlertTooFast = targetPace * (100 - paceMarginPercent) / 100;	/* these are minutes/km, so the lower the value, the faster you run */
+			paceAlertTooSlow = targetPace * (100 + paceMarginPercent) / 100;	/* ...and vice-versa ;-) */
+
+			myDistanceKm = SUUNTO_DISTANCE;
+			}
+		}
+	else {
+		/* NOT YET */
+		secondsLeft = endOfStepSeconds - SUUNTO_DURATION;
+		prefix = "WUP";	/* 'Warm up' */
+		myResultVar = secondsLeft;
+		postfix = "S";	/* 'seconds' */
 		}
 	}
 
-else if (step==1 || step==3 || step==5) {
-	/* RUN */
+
+/*******
+ * RUN *
+ ******/
+else if (step == 1 || step == 3 || step == 5) {
+
+	endOfStepKm = myDistanceKm + runLengthMeters / 1000;
 
 	/* IS THIS RUN OVER ? */
-	if (SUUNTO_DISTANCE - myDistanceKm < fastRunLengthMeters/1000) {
-		/* NOT YET */
-		prefix="";
-		myResultVar=runId;
-
-		/* PACE MONITORING */
-		postfix="RUN";
-		if (SUUNTO_PACE>paceAlertTooSlow) { postfix="RUN+"; }
-		if (SUUNTO_PACE<paceAlertTooFast) { postfix="RUN-"; }
-		}
-	else {
+	if (SUUNTO_DISTANCE > endOfStepKm) {
 		/* YES : RUN IS OVER */
 		Suunto.alarmBeep();
-		step=step+1;
-		runId=runId+1;
+		step = step + 1;
+		runId = runId + 1;
+		myDurationSeconds = SUUNTO_DURATION;
 		/*
-		SUUNTO_LAP_NUMBER=SUUNTO_LAP_NUMBER+1;
+		SUUNTO_LAP_NUMBER = SUUNTO_LAP_NUMBER + 1;
 		This would be great but is not supported so far :-(
 		*/
-		myDurationSeconds=SUUNTO_DURATION;
-		myDistanceKm=SUUNTO_DISTANCE;
-		}
-	}
-
-else if (step==2 || step==4) {
-	/* REST */
-
-	/* IS THIS REST OVER ? */
-	if (SUUNTO_DURATION - myDurationSeconds < restBetweenRunsSeconds) {
-		/* NOT YET */
-		restTimeLeft=restBetweenRunsSeconds-SUUNTO_DURATION+myDurationSeconds;
-		prefix="R";
-		myResultVar=restTimeLeft;
-		postfix="s";
 		}
 	else {
-		/* YES : REST IS OVER */
-		myResultVar=step;
-		Suunto.alarmBeep();
-		step=step+1;
-		/*
-		SUUNTO_LAP_NUMBER=SUUNTO_LAP_NUMBER+1;
-		This would be great but is not supported so far :-(
-		*/
-		myDurationSeconds=SUUNTO_DURATION;
-		myDistanceKm=SUUNTO_DISTANCE;
+		/* NOT YET */
+		prefix = "RUN";
+		myResultVar = runId;
+
+		/* PACE MONITORING */
+		postfix = "";
+		if (SUUNTO_PACE > paceAlertTooSlow) { postfix = "++"; }
+		if (SUUNTO_PACE < paceAlertTooFast) { postfix = "--"; }
 		}
 	}
 
-else if (step>5) {
-	/* CALM DOWN */
-	prefix="";
-	myResultVar=step;
-	postfix="CALM";
+
+/********
+ * REST *
+ *******/
+else if (step == 2 || step == 4) {
+
+	endOfStepSeconds = myDurationSeconds + restBetweenRunsSeconds;
+
+	/* IS THIS REST OVER ? */
+	if (SUUNTO_DURATION > endOfStepSeconds) {
+		/* YES : REST IS OVER */
+		Suunto.alarmBeep();
+		step = step + 1;
+		myDistanceKm = SUUNTO_DISTANCE;
+		/*
+		SUUNTO_LAP_NUMBER = SUUNTO_LAP_NUMBER + 1;
+		This would be great but is not supported so far :-(
+		*/
+		}
+	else {
+		/* NOT YET */
+		secondsLeft = endOfStepSeconds - SUUNTO_DURATION;
+		prefix = "RST";
+		myResultVar = secondsLeft;
+		postfix = "S";
+		}
+	}
+
+
+/*************
+ * CALM DOWN *
+ *************/
+else if (step > 5) {
+	prefix = "CALM";
 	}
 
 /* THE END */
