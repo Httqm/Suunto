ea50721401d53f3c6e00d9a57b0e758657e7feb9
Matthieu FOURNET <matthieu.fournet@orange.com>
2019-03-25 17:45:22 +0100

    ADD "difficulty"

--- Pilates.js
+++ Pilates.js
@@ -34,11 +34,11 @@
 # VARIABLES :
 #
 # difficultyPercent					= 100	CAN BE EDITED
+# readyCountdownSeconds				= 10	CAN BE EDITED
+# restBetweenExercicesSeconds		= 20	CAN BE EDITED
+#
 # fullDurationSeconds				= 60	DON'T EDIT
 # halfDurationSeconds				= 30	DON'T EDIT
-# restBetweenExercicesSeconds		= 20	DON'T EDIT
-#
-# readyCountdownSeconds				= 10	DON'T EDIT
 # endOfStepSeconds					= 0		DON'T EDIT
 # step								= 0		DON'T EDIT
 #
@@ -55,6 +55,12 @@
  * "Ready ?" countdown *
  **********************/
 if (step < 1) {
+
+	/* compute durations "with difficulty" */
+	fullDurationSeconds_difficult			= round(fullDurationSeconds * difficultyPercent / 100);
+	halfDurationSeconds_difficult			= round(halfDurationSeconds * difficultyPercent / 100);
+	restBetweenExercicesSeconds_difficult	= round(restBetweenExercicesSeconds * difficultyPercent / 100);
+
 	endOfStepSeconds = readyCountdownSeconds;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
@@ -69,7 +75,7 @@ if (step < 1) {
  * Full plank 1 *
  ***************/
 else if (step == 1) {
-	endOfStepSeconds = myDurationSeconds + fullDurationSeconds;
+	endOfStepSeconds = myDurationSeconds + fullDurationSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
@@ -83,7 +89,7 @@ else if (step == 1) {
  * REST *
  *******/
 else if (mod(step,2)==0 && step>=2 && step<15) {
-	endOfStepSeconds = myDurationSeconds + restBetweenExercicesSeconds;
+	endOfStepSeconds = myDurationSeconds + restBetweenExercicesSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
@@ -97,7 +103,7 @@ else if (mod(step,2)==0 && step>=2 && step<15) {
  * Elbow plank 1 *
  ****************/
 else if (step == 3) {
-	endOfStepSeconds = myDurationSeconds + halfDurationSeconds;
+	endOfStepSeconds = myDurationSeconds + halfDurationSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
@@ -111,7 +117,7 @@ else if (step == 3) {
  * Raised leg plank (leg1) *
  **************************/
 else if (step == 5) {
-	endOfStepSeconds = myDurationSeconds + halfDurationSeconds;
+	endOfStepSeconds = myDurationSeconds + halfDurationSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
@@ -125,7 +131,7 @@ else if (step == 5) {
  * Raised leg plank (leg2) *
  **************************/
 else if (step == 7) {
-	endOfStepSeconds = myDurationSeconds + halfDurationSeconds;
+	endOfStepSeconds = myDurationSeconds + halfDurationSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
@@ -139,7 +145,7 @@ else if (step == 7) {
  * Side plank (side1) *
  *********************/
 else if (step == 9) {
-	endOfStepSeconds = myDurationSeconds + halfDurationSeconds;
+	endOfStepSeconds = myDurationSeconds + halfDurationSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
@@ -153,7 +159,7 @@ else if (step == 9) {
  * Side plank (side2) *
  *********************/
 else if (step == 11) {
-	endOfStepSeconds = myDurationSeconds + halfDurationSeconds;
+	endOfStepSeconds = myDurationSeconds + halfDurationSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
@@ -167,7 +173,7 @@ else if (step == 11) {
  * Full plank 2 *
  ***************/
 else if (step == 13) {
-	endOfStepSeconds = myDurationSeconds + halfDurationSeconds;
+	endOfStepSeconds = myDurationSeconds + halfDurationSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
@@ -181,7 +187,7 @@ else if (step == 13) {
  * Elbow plank 2 *
  ****************/
 else if (step == 15) {
-	endOfStepSeconds = myDurationSeconds + fullDurationSeconds;
+	endOfStepSeconds = myDurationSeconds + fullDurationSeconds_difficult;
 	if (SUUNTO_DURATION > endOfStepSeconds) {
 		Suunto.alarmBeep();
 		step = step + 1;
